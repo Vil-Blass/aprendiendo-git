7117da697d3a940d87d4f68ccc5ab2151968052a
Vil-Blass <131159959+Vil-Blass@users.noreply.github.com>
2023-04-23 20:16:30 +0200

    declaraciones de preguntas y respuestas

---
+++ jasc.js
@@ -0,0 +1,41 @@
+var pregHTML = [
+
+    "1.¿Cuál es el element HTML que ese utiliza para definir un encabezado de página?",
+
+    "2. ¿Cuál es el atributo HTML que se utiliza para definir el texto alternativo de una imagen?",
+
+    "3. ¿Cuál es el elemento HTML que se utiliza para crear una lista no ordenada?",
+
+    "4.¿Cuál es el atributo HTML que se utiliza para especificar la dirección de lectura del texto?",
+
+    "5. ¿Cuál es el atributo HTML que se utiliza para especificar el tipo de documento?",
+
+    "6. ¿Qué significa el acrónimo HTML?",
+
+    "7.  ¿Qué etiqueta es correcta para indicar un salto de línea? ",
+
+    "8.  ¿Cuál es la forma correcta de escribir un enlace en HTML?",
+
+    "9. ¿Qué etiqueta se usa para denominar cada fila en una tabla html?",
+
+    "10.¿Cómo harías para que se abriera el enlace en la misma pestaña en la que nos encontramos?",
+
+];
+
+
+//las 4 respuestas
+
+var respuesta1 = ['h1','alt','<ol>', 'dir', 'type', 'Hyper Text Mask Language', '</br>', '<a title=» «>…</>', '<ol>', '<link href=»#» target=»_blank»>…</link>']
+
+    
+var respuesta2=['<title>','src','<li>','lang', 'class', 'Hard Text Markup Language', '<br/>', '<a href=» «>…</a>', '<tr>', ' <link href=»#» target=»_self»>…</link>']
+
+
+var respuesta3=['<head>', 'href', '<ul>', 'class', 'lang', 'Hyper Text Markup Language', ' <breakLine>', ' <a src=» «>…</a>', '<li>', '<a href=»#» target=»_blank»>…</a>']
+
+
+var respuesta4=['<header>','title', '<list>',' id', 'doctype', 'Hard Text Mask Language','<hr>', '<link href=» «>…<link>', '<list>', '<a href=»#» target=»_self»>…</a>']
+
+
+
+//las respuestas correctas
